69e3e5cc381986199be9c4a4b0490f7665e97428
AgungPremaditya <premadityamandala@gmail.com>
2022-05-26 23:10:38 +0800

    add crud store

--- controllers/StoreController.ts
+++ controllers/StoreController.ts
@@ -1,54 +1,56 @@
 import { PrismaClient } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
+import * as argon2 from "argon2";
 
 const prisma = new PrismaClient();
 
-export default class CategoryController {
+export default class StoreController {
   static async get(req: Request, res: Response) {
-    const categories = await prisma.category.findMany();
+    const stores = await prisma.store.findMany();
 
-    return res.status(200).json(categories);
+    return res.status(200).json(stores);
   }
 
   static async store(req: Request, res: Response, next: NextFunction) {
-    const { name, detail } = req.body;
+    const { password, ...store } = req.body;
+
+    const hashPassword = await argon2.hash(password);
 
     try {
-      const category = await prisma.category.create({
-        data: { name, detail },
+      const result = await prisma.store.create({
+        data: { password: hashPassword, ...store },
       });
 
-      return res.status(201).json(category);
+      return res.status(201).json(result);
     } catch (error) {
       next(error);
     }
   }
 
-  static async show(req: Request, res: Response) {
-    const category = await prisma.category.findUnique({
+  static async show(req: Request, res: Response, next: NextFunction) {
+    const result = await prisma.store.findUnique({
       where: {
         id: req.params.id,
       },
     });
 
-    return res.status(200).json(category);
+    return res.status(200).json(result);
   }
 
   static async update(req: Request, res: Response, next: NextFunction) {
-    const { name, detail } = req.body;
+    const { password, ...store } = req.body;
+
+    const hashPassword = await argon2.hash(password);
 
     try {
-      const category = await prisma.category.update({
-        data: {
-          name,
-          detail,
-        },
+      const result = await prisma.store.update({
+        data: { password: hashPassword, ...store },
         where: {
           id: req.params.id,
         },
       });
 
-      res.status(200).json(category);
+      return res.status(200).json(result);
     } catch (error) {
       next(error);
     }
@@ -56,7 +58,7 @@ export default class CategoryController {
 
   static async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      await prisma.category.delete({
+      await prisma.store.delete({
         where: {
           id: req.params.id,
         },
--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Request, Response, NextFunction } from "express";
 
 import CategoryController from "../controllers/CategoryController";
+import StoreController from "../controllers/StoreController";
 
 import { ValidateCategory } from "../validators/CategoryValidator";
 
@@ -17,5 +18,12 @@ router.post("/categories", ValidateCategory, CategoryController.store);
 router.get("/categories/:id", CategoryController.show);
 router.put("/categories/:id", CategoryController.update);
 router.delete("/categories/:id", CategoryController.delete);
+
+// Store
+router.get("/stores", StoreController.get);
+router.post("/stores", StoreController.store);
+router.get("/stores/:id", StoreController.show);
+router.put("/stores/:id", StoreController.update);
+router.delete("/stores/:id", StoreController.delete);
 
 export default router;
